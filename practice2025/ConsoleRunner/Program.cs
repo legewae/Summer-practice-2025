276b73da2470458052d73c8741b5cf9960037ed9
legewae <legendaryway0@gmail.com>
2025-07-12 11:51:49 +0600

    feat: task09

---
+++ practice2025/ConsoleRunner/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Reflection;
+
+namespace ConsoleRunner
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Передайте путь к библиотеке!");
+                return;
+            }
+
+            string assemblyPath = args[0];
+            if (!System.IO.File.Exists(assemblyPath))
+            {
+                Console.WriteLine("Файл не найден: " + assemblyPath);
+                return;
+            }
+
+            var assembly = Assembly.LoadFrom(assemblyPath);
+
+            Console.WriteLine($"Динамическая библиотека {assembly.FullName}");
+            foreach(var type in assembly.GetTypes())
+            {
+                Console.WriteLine($"Класс: {type.Name}");
+
+                Console.WriteLine("Методы класса:");
+                foreach(var method in type.GetMethods())
+                {
+                    Console.WriteLine($"Метод {method.Name}");
+                    Console.WriteLine("Параметры метода:");
+                    foreach(var par in method.GetParameters())
+                    {
+                        Console.WriteLine($"{par.Name} типа {par.ParameterType.Name}");
+                    }
+                }
+
+                Console.WriteLine("Свойства класса:");
+                foreach (var prop in type.GetProperties())
+                {
+                    Console.WriteLine($"Свойство {prop.Name} типа {prop.PropertyType.Name}");
+                }
+
+                Console.WriteLine("Конструкторы класса:");
+                foreach(var constructor in type.GetConstructors())
+                {
+                    Console.WriteLine($"Конструктор {constructor.Name}");
+                    Console.WriteLine("Параметры конструктора:");
+                    foreach (var par in constructor.GetParameters())
+                    {
+                        Console.WriteLine($"{par.Name} типа {par.ParameterType.Name}");
+                    }
+                }
+
+                Console.WriteLine("Аттрибуты класса:");
+                foreach(var att in type.GetCustomAttributes())
+                {
+                    Console.WriteLine($"Атрибут: {att.GetType().Name}");
+                }
+                
+            }
+        }
+    }
+}
--- practice2025/task07tests/UnitTest1.cs
+++ practice2025/task07tests/UnitTest1.cs
@@ -1,45 +1,124 @@
-﻿using System.Reflection;
-using System.Text;
-using task07;
-using Xunit;
+﻿    using System.Reflection;
+    using System.Text;
+    using task07;
+    using Xunit;
+    using ConsoleRunner;
 
-public class AttributeReflectionTests
-{
-
-    [Fact]
-    public void Class_HasDisplayNameAttribute()
+    public class AttributeReflectionTests
     {
-        var type = typeof(SampleClass);
-        var attribute = type.GetCustomAttribute<DisplayNameAttribute>();
-        Assert.NotNull(attribute);
-        Assert.Equal("Пример класса", attribute.DisplayName);
+
+        [Fact]
+        public void Class_HasDisplayNameAttribute()
+        {
+            var type = typeof(SampleClass);
+            var attribute = type.GetCustomAttribute<DisplayNameAttribute>();
+            Assert.NotNull(attribute);
+            Assert.Equal("Пример класса", attribute.DisplayName);
+        }
+
+        [Fact]
+        public void Method_HasDisplayNameAttribute()
+        {
+            var method = typeof(SampleClass).GetMethod("TestMethod");
+            var attribute = method.GetCustomAttribute<DisplayNameAttribute>();
+            Assert.NotNull(attribute);
+            Assert.Equal("Тестовый метод", attribute.DisplayName);
+        }
+
+        [Fact]
+        public void Property_HasDisplayNameAttribute()
+        {
+            var prop = typeof(SampleClass).GetProperty("Number");
+            var attribute = prop.GetCustomAttribute<DisplayNameAttribute>();
+            Assert.NotNull(attribute);
+            Assert.Equal("Числовое свойство", attribute.DisplayName);
+        }
+
+        [Fact]
+        public void Class_HasVersionAttribute()
+        {
+            var type = typeof(SampleClass);
+            var attribute = type.GetCustomAttribute<VersionAttribute>();
+            Assert.NotNull(attribute);
+            Assert.Equal(1, attribute.Major);
+            Assert.Equal(0, attribute.Minor);
+        }
+
+        [Fact]
+        public void DLL_NoPathToLibrary()
+        {
+
+            var writer = new StringWriter();
+
+            Console.SetOut(writer);
+
+            ConsoleRunner.Program.Main(new string[] {});
+
+            var output = writer.ToString();
+
+            Assert.Contains("Передайте путь к библиотеке!", output);
     }
 
-    [Fact]
-    public void Method_HasDisplayNameAttribute()
-    {
-        var method = typeof(SampleClass).GetMethod("TestMethod");
-        var attribute = method.GetCustomAttribute<DisplayNameAttribute>();
-        Assert.NotNull(attribute);
-        Assert.Equal("Тестовый метод", attribute.DisplayName);
+        [Fact]
+        public void DLL_ChecksClasses()
+        {
+
+            var writer = new StringWriter();
+
+            Console.SetOut(writer);
+
+            ConsoleRunner.Program.Main(new string[] { "task07.dll" });
+
+            var output = writer.ToString();
+
+            Assert.Contains("Класс: SampleClass", output);
+            Assert.Contains("Класс: DisplayNameAttribute", output);
+            Assert.Contains("Класс: VersionAttribute", output);
+            Assert.Contains("Класс: ReflectionHelper",output);
     }
 
-    [Fact]
-    public void Property_HasDisplayNameAttribute()
-    {
-        var prop = typeof(SampleClass).GetProperty("Number");
-        var attribute = prop.GetCustomAttribute<DisplayNameAttribute>();
-        Assert.NotNull(attribute);
-        Assert.Equal("Числовое свойство", attribute.DisplayName);
-    }
+        [Fact]
+        public void DLL_ChecksMethods()
+        {
 
+            var writer = new StringWriter();
+
+            Console.SetOut(writer);
+        
+            ConsoleRunner.Program.Main(new string[] { "task07.dll" });
+
+            var output = writer.ToString();
+
+            Assert.Contains("Метод TestMethod",output);
+            Assert.Contains("Метод PrintTypeInfo",output);
+        }
+
+        [Fact]
+        public void DLL_ContainsConstructors()
+            {
+
+            var writer = new StringWriter();
+
+            Console.SetOut(writer);
+
+            ConsoleRunner.Program.Main(new string[] { "task07.dll" });
+
+            var output = writer.ToString();
+
+            Assert.Contains("Конструктор", output);
+        }
     [Fact]
-    public void Class_HasVersionAttribute()
+    public void DLL_ContainsAttributes()
     {
-        var type = typeof(SampleClass);
-        var attribute = type.GetCustomAttribute<VersionAttribute>();
-        Assert.NotNull(attribute);
-        Assert.Equal(1, attribute.Major);
-        Assert.Equal(0, attribute.Minor);
+
+        var writer = new StringWriter();
+
+        Console.SetOut(writer);
+
+        ConsoleRunner.Program.Main(new string[] { "task07.dll" });
+
+        var output = writer.ToString();
+
+        Assert.Contains("Атрибут:", output);
     }
 }
