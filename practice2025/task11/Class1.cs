6afdadd9ea03f8e79bf6060e2c1f3dfdcd72093e
legewae <legendaryway0@gmail.com>
2025-07-14 14:54:13 +0600

    feat: task11

---
+++ practice2025/task11/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.CSharp;
+using System.CodeDom.Compiler;
+using System.Reflection;
+using System.CodeDom;
+
+public class Calc
+{
+    public object CreateCalculator()
+    {
+        string classCode = @"public class Calculator
+        {
+        public int Add(int a, int b) => a + b;
+        public int Minus(int a, int b) => a - b;
+        public int Mul(int a, int b) => a * b;
+        public int Div(int a, int b) => a / b;
+        }";
+
+        CSharpCodeProvider compiler = new CSharpCodeProvider();
+        CompilerParameters parameters = new CompilerParameters
+        {
+            GenerateInMemory = true,
+            GenerateExecutable = false
+        };
+
+        CompilerResults results = compiler.CompileAssemblyFromSource(parameters, classCode);
+
+        return Activator.CreateInstance(results.CompiledAssembly.GetType("Calculator"));
+    }
+}
